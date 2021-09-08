8493dd688f44035634497419376e7c994857cda5
Maks Lobarev <simaks147@gmail.com>
2021-09-08 12:28:21 +0300

    1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+  Array.prototype.forEach.call(arguments, (el) => {
+    if (typeof el !== 'number') throw new TypeError();
+  });
+
+  return a + b;
 }
 
 module.exports = sum;
