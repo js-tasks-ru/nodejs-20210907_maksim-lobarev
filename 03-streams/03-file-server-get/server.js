c3dbcf2d2e9f6a324899255086784445b690d590
Maks Lobarev <simaks147@gmail.com>
2021-09-21 15:11:14 +0300

    change status code from 404 to 400

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -6,27 +6,27 @@ const fs = require('fs');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = url.pathname.slice(1);
+
+  if (pathname.indexOf('/') !== -1) {
+    res.statusCode = 400;
+    res.end('No such file');
+  }
+
+  const filepath = path.join(__dirname, 'files', pathname);
+
   switch (req.method) {
     case 'GET':
-      const url = new URL(req.url, `http://${req.headers.host}`);
-      const pathname = url.pathname.slice(1);
-
-      if (pathname.indexOf('/') !== -1) {
-        res.statusCode = '404';
-        res.end('No such file');
-      }
-
-      const filepath = path.join(__dirname, 'files', pathname);
-
       const readStream = fs.createReadStream(filepath);
 
       readStream.on('error', err => {
         if (err.code === 'ENOENT') {
-          res.statusCode = '404';
+          res.statusCode = 404;
           res.end('No such file');
         }
         else {
-          res.statusCode = '500';
+          res.statusCode = 500;
           res.end('Server Error');
         }
       });
