ee2fae0eda3a3f55664a5b5b76db4cc292d8cf7e
Maks Lobarev <simaks147@gmail.com>
2021-09-21 15:22:28 +0300

    3-module 5-task

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -8,10 +9,31 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('No such file');
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      fs.unlink(filepath, err => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('No such file');
+          }
+          else {
+            res.statusCode = 500;
+            res.end('Server Error');
+          }
+
+          return;
+        }
+
+        res.end('Delete done');
+      });
 
       break;
 
