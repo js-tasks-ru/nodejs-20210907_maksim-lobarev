4ad79e0e1b783e76fb0720afd3d34ee16a5f5591
Maks Lobarev <simaks147@gmail.com>
2021-09-21 15:17:13 +0300

    3-module 4-task

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,9 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -8,10 +11,59 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('No such file');
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
+      const writeStream = fs.createWriteStream(filepath, {
+        flags: 'wx'
+      });
+
+      const limitSizeStream = new LimitSizeStream({limit: 10000});
+
+      req.pipe(limitSizeStream).pipe(writeStream);
+
+      limitSizeStream.on('error', error => {
+        if (error.code === 'LIMIT_EXCEEDED') {
+          res.statusCode = 413;
+          res.end(error.message);
+        }
+        else {
+          res.statusCode = 500;
+          res.end('Server error');
+        }
+
+        writeStream.destroy();
+        fs.unlink(filepath, () => {});
+      });
+
+      writeStream.on('error', error => {
+        if (error.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('File already exists');
+        }
+        else {
+          res.statusCode = 500;
+          res.end('Server error');
+        }
+      });
+
+      writeStream.on('finish', () => {
+        res.statusCode = 201;
+        res.end('File created')
+      });
+
+      req.on('aborted', () => {
+        limitSizeStream.destroy();
+        writeStream.destroy();
+
+        fs.unlink(filepath, () => {});
+      });
 
       break;
 
