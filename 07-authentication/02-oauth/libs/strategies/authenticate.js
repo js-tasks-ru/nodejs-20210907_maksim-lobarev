0b48ba1d13ee683dc7d31d540e45c7dab26d27dd
Maks Lobarev <simaks147@gmail.com>
2021-10-11 16:27:50 +0300

    7-module 2-task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,19 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (email) {
+    const user = await User.findOne({email});
+
+    if (!user) {
+      User.create({email, displayName})
+        .then(newUser => done( null, newUser ))
+        .catch(err => done(err));
+    }
+    else {
+      done(null, user);
+    }
+  }
+  else {
+    done(null, false, 'Не указан email');
+  }
 };
